15bd32acf38204c7c40afc3605c9dd9099d633aa
Tomy Budiman <tomybudiman@protonmail.com>
2026-04-14 17:00:08 +0700

    refactor: update package namespace and application ID to 'com.journify.app'; rename MainActivity and MainApplication files

--- app/android/app/src/main/java/com/journify/app/MainActivity.kt
+++ app/android/app/src/main/java/com/journify/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.journify
+package com.journify.app
 
 import android.os.Bundle
 import com.facebook.react.ReactActivity
--- app/android/app/src/main/java/com/journify/app/MainApplication.kt
+++ app/android/app/src/main/java/com/journify/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.journify
+package com.journify.app
 
 import android.app.Application
 import com.facebook.react.PackageList
